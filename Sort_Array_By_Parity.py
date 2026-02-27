f2dea517c5dfc79a5f578a1ef384ef0aa2be76f9
Akash Rathod <akash.rathod162844@gmail.com>
2026-02-27 22:25:36 +0530

    Create Sort_Array_By_Parity.py

---
+++ Sort_Array_By_Parity.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        a = []
+        for i in nums:
+            if i % 2 ==0:
+                a.append(i)
+        for i in nums:
+            if i % 2 != 0:
+                a.append(i)
+        return a
