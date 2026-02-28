f81f852444a9c61800a976d3df2dfbbaab319851
Akash Rathod <akash.rathod162844@gmail.com>
2026-02-28 22:47:43 +0530

    Create Length_of_last_word.py

---
+++ Length_of_last_word.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        
+    
+        j = len(s) - 1
+        length = 0
+        
+        
+        while j >= 0 and s[j] == " ":
+            j -= 1
+        
+        
+        while j >= 0 and s[j] != " ":
+            length += 1
+            j -= 1
+        
+        return length
+                
