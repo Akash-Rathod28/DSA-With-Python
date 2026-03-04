7bc966c7e308d47edb43bcc3c15d6f6f375b3742
Akash Rathod <akash.rathod162844@gmail.com>
2026-03-04 21:36:23 +0530

    Create Add_digits.py

---
+++ Add_digits.py
@@ -0,0 +1,21 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        # if len(str(num)) == 1:
+        #     return num
+        # while True:
+        #     s = str(num)
+        #     sum1 = 0
+            
+        #     for i in range(len(s)):  
+        #         sum1 = sum1 + int(s[i])
+            
+        #     if len(str(sum1)) == 1:  
+        #         return sum1
+        #     else:
+        #         num = sum1    
+
+
+        if num == 0:
+            return 0
+        return 1 + (num - 1) % 9
+        
