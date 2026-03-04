ad6a1fff83087250cdb0116e9117d1c4ba67e1d6
Akash Rathod <akash.rathod162844@gmail.com>
2026-03-04 19:52:52 +0530

    Create Selection_Sort.py

---
+++ Selection_Sort.py
@@ -0,0 +1,14 @@
+class Solution:
+  def sortArray(self,nums:List[int]) -> List[int]:
+    n = len(nums)
+    for i in range(n):
+      mn = nums[]
+      ind = i
+      for j in range(i+1,n):
+        if nums[j] <mn:
+          mn = nums[j]
+          ind = j
+      temp = nums[i]
+      nums[i] = nums[ind]
+      nums[ind] = temp
+    return nums
