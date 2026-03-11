eb319a082f8b0853b2f78446b630054ca5fd70d1
Akash Rathod <akash.rathod162844@gmail.com>
2026-03-11 19:59:20 +0530

    Create SQRT(X).py

---
+++ SQRT(X).py
@@ -0,0 +1,21 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+
+    # one method
+        #return floor(sqrt(x))
+
+    # another method
+        if x == 0:
+            return 0
+        l = 1
+        r = x
+        ans = 1
+        while l <= r:
+            mid = (l+r) // 2
+            midsquare = mid * mid
+            if midsquare > x:
+                r = mid - 1
+            else:
+                ans = mid
+                l = mid + 1
+        return ans
