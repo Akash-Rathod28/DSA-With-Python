79484d5161cf390077d02eee372fd54998f91fc0
Akash Rathod <akash.rathod162844@gmail.com>
2026-03-24 07:15:51 +0530

    Update Running_Sum_of_1dArray.py

--- Running_Sum_of_1dArray.py
+++ Running_Sum_of_1dArray.py
@@ -8,9 +8,19 @@ class Solution:
         #     lis.append(x)
         #     i+=1
         # return lis
+
+
+        # ans = []
+        # ans.append(nums[0])
+        # for i in range(1,len(nums)):
+        #     x = ans[i-1] + nums[i]
+        #     ans.append(x)
+        # return ans
+
+
+
         ans = []
-        ans.append(nums[0])
-        for i in range(1,len(nums)):
-            x = ans[i-1] + nums[i]
-            ans.append(x)
+        for i in range(1,len(nums) + 1):
+           a = sum(nums[:i])
+           ans.append(a)
         return ans
