7cc5e7389e825291e4ed751f36a0f27a5c220330
Akash Rathod <akash.rathod162844@gmail.com>
2026-03-26 07:53:36 +0530

    Update Two_sum.py

--- Two_sum.py
+++ Two_sum.py
@@ -1,16 +1,38 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
        
         
         # for i in range(len(nums)):
         #     for j in range(i):
         #         if nums[i] + nums[j] == target:
         #             return [j, i]
-        hash1 = {}
+
+        
+        # hash1 = {}
+        # for i in range(len(nums)):
+        #     rem = target - nums[i]
+        #     if rem in hash1:
+        #         return [hash1[rem],i]
+        #     hash1[nums[i]] = i
+
+
+        # class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i,j]   
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        
         for i in range(len(nums)):
-            rem = target - nums[i]
-            if rem in hash1:
-                return [hash1[rem],i]
-            hash1[nums[i]] = i
+            complement = target - nums[i]
+            
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            
+            hashmap[nums[i]] = i
        
                 
