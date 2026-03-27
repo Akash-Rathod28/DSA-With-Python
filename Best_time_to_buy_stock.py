4fb04bf6bb7276a53d12961618aca211f25a45c6
Akash Rathod <akash.rathod162844@gmail.com>
2026-03-27 07:53:27 +0530

    Update Best_time_to_buy_stock.py

--- Best_time_to_buy_stock.py
+++ Best_time_to_buy_stock.py
@@ -1,11 +1,25 @@
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         min_price = prices[0]
+#         profit = 0
+
+#         for i in range(1,len(prices)):
+#             curr_profit = prices[i] - min_price
+#             if curr_profit > profit:
+#                 profit = curr_profit
+#             min_price = min(min_price,prices[i])
+#         return profit
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         min_price = prices[0]
-        profit = 0
-
-        for i in range(1,len(prices)):
-            curr_profit = prices[i] - min_price
-            if curr_profit > profit:
-                profit = curr_profit
-            min_price = min(min_price,prices[i])
-        return profit
+        max_profit = 0
+        
+        for i in range(1, len(prices)):
+            profit = prices[i] - min_price
+            max_profit = max(max_profit, profit)
+            
+            min_price = min(min_price, prices[i])
+        
+        return max_profit
