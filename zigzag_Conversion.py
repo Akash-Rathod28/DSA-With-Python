6747a7664f8ce837d2985f86deb6a370f888331d
Akash Rathod <akash.rathod162844@gmail.com>
2026-03-30 08:37:49 +0530

    Create zigzag_Conversion.py

---
+++ zigzag_Conversion.py
@@ -0,0 +1,19 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        i = 0
+        d = 1
+        rows = [[] for _ in range(numRows)]
+        for ch in s:
+            rows[i].append(ch)
+            if i == 0:
+                d = 1
+            elif i == numRows - 1:
+                d = -1
+            i += d
+        ret = ""
+        for i in range(numRows):
+            ret += "".join(rows[i])
+        return ret
+        
