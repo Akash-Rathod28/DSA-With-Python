b62062890fb935d53b773224c51bc426f68ac76c
Akash Rathod <akash.rathod162844@gmail.com>
2026-04-15 07:52:26 +0530

    Create Partitioning Into Minimum Number Of Deci-Binary Numbers.py

---
+++ Partitioning Into Minimum Number Of Deci-Binary Numbers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return int(max(n))
+        # a = 0
+        # for i in n:
+        #     if int(i) > a:
+        #         a = int(i)
+        # return int(a)
