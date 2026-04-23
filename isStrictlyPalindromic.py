e921ae91af76991607c8aaf86af3ced180b56229
Akash Rathod <akash.rathod162844@gmail.com>
2026-04-23 07:56:22 +0530

    Create isStrictlyPalindromic.py

---
+++ isStrictlyPalindromic.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        # a = bin(n)[2:]
+        # s = a[::-1]
+        # # for i in range(len(a)-1):
+        # #     s += str(i)
+        # b = a[:len(a)-1]
+        # if s == b:
+        #     return True
+        # else:
+        #     return False
+        
+        return False
