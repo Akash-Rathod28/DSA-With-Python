96c315405df264ffe459935c09d0bf8cdcbd08f5
Akash Rathod <akash.rathod162844@gmail.com>
2026-05-15 18:33:53 +0530

    Create numOfStrings.py

---
+++ numOfStrings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numOfStrings(self, patterns: List[str], word: str) -> int:
+        count = 0
+        for val in patterns:
+            if val in word:
+                count += 1
+        return count
+                        
+        
