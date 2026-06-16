85c4c872d664eeeae2d3501df403fab31cc5fa38
Akash Rathod <akash.rathod162844@gmail.com>
2026-06-16 07:50:59 +0530

    Add string processing method in Solution class
    Implement a string processing method that handles alphabetic characters, duplicates, reversals, and deletions based on specific symbols.

---
+++ processStr.py
@@ -0,0 +1,14 @@
+class Solution:
+    def processStr(self, s: str) -> str:
+        result = ""
+        for char in s:
+            if char.isalpha():
+                result += char
+            elif char == "#":
+                result += result
+            elif char == "%":
+                result = result[::-1]
+            # elif char == "*":
+            else:
+                result = result[:len(result)-1]
+        return result        
