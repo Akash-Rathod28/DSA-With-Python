3fff9134bb59fc1e6cc66e56390573e747d9421c
Akash Rathod <akash.rathod162844@gmail.com>
2026-09-11 06:19:14 +0530

    Create totalNumbers.py

---
+++ totalNumbers.py
@@ -0,0 +1,18 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+
+  def totalNumbers(self, digits: List[int]) -> int:
+    counts = Counter(digits)
+    ans = 0
+
+    for num in range(100, 1000, 2):
+      d1, d2, d3 = num // 100, (num // 10) % 10, num % 10
+      num_counts = Counter([d1, d2, d3])
+
+      if all(counts[d] >= num_counts[d] for d in num_counts):
+        ans += 1
+
+    return ans
