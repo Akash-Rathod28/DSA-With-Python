403fe97a43369fb075b676e009652b85c018674a
Akash Rathod <akash.rathod162844@gmail.com>
2026-09-15 06:15:53 +0530

    Implement maxPalindromes method to count palindromes

---
+++ maxPalindromes.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxPalindromes(self, s: str, k: int) -> int:
+        n = len(s)
+        dp = [0] * (n + 1)
+        
+        for i in range(n):
+            dp[i+1] = max(dp[i+1], dp[i])
+            # Expand for odd and even length palindromes centered around/touching i
+            for l, r in [(i, i), (i, i+1)]:
+                while l >= 0 and r < n and s[l] == s[r]:
+                    if r - l + 1 >= k:
+                        dp[r+1] = max(dp[r+1], dp[l] + 1)
+                    l -= 1
+                    r += 1
+                    
+        return dp[n]
